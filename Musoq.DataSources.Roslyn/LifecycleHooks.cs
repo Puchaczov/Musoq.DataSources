a003ada98ccea9eef94cfba4a0015f0ca560a0de
puchacz <puchala.czwa@gmail.com>
2025-01-19 21:31:21 +0100

    Runtime update version

--- Musoq.DataSources.Roslyn/LifecycleHooks.cs
+++ Musoq.DataSources.Roslyn/LifecycleHooks.cs
@@ -5,7 +5,6 @@ using System.Threading;
 using System.Threading.Tasks;
 using ConsoleAppFramework;
 using Microsoft.Build.Locator;
-using Microsoft.Extensions.DependencyInjection;
 using Musoq.DataSources.Roslyn.CoconaCommands;
 
 namespace Musoq.DataSources.Roslyn;
