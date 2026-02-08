ffbdb1c809282a7b0b82dcaec616ab925ecb5997
Puchaczov <6973258+Puchaczov@users.noreply.github.com>
2026-02-08 22:16:23 +0100

    feat: Add missing GitHub releases source files.

---
+++ Musoq.DataSources.GitHub/Sources/Releases/ReleasesTable.cs
@@ -0,0 +1,21 @@
+using Musoq.DataSources.GitHub.Entities;
+using Musoq.Schema;
+
+namespace Musoq.DataSources.GitHub.Sources.Releases;
+
+internal class ReleasesTable : ISchemaTable
+{
+    public ISchemaColumn? GetColumnByName(string name)
+    {
+        return Columns.SingleOrDefault(column => column.ColumnName == name);
+    }
+
+    public ISchemaColumn[] GetColumnsByName(string name)
+    {
+        return Columns.Where(column => column.ColumnName == name).ToArray();
+    }
+
+    public ISchemaColumn[] Columns => ReleasesSourceHelper.ReleasesColumns;
+    
+    public SchemaTableMetadata Metadata { get; } = new(typeof(ReleaseEntity));
+}
